10712dfae2fcb302a35cd4134c6e3c408bcefd69
Anoop Varghese <anoop@metaboard.io>
2016-06-27 22:04:17 +1000

    destructure

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ function* fibonacci(limit = Infinity) {
     yield next;
   }
 }
-
+// iterator
+console.log('=========Iterator===========');
 for(let i of fibonacci(100)) {
-  console.log(i)
+  console.log(i);
 }
 
 //or call as below
-
+console.log('=========Spread operator===========');
 console.log([...fibonacci(200)]);
+
+
+// concate arrays in es6
+console.log('=========Array concat===========');
+console.log([...[1,2],...[3,4]]);
+
+// to arguments
+console.log('=========Arguments===========');
+console.log(...fibonacci(30));
+
+// some more destructuring
+console.log('=========Destructuring===========');
+const [a,...b] = [...fibonacci(30)]
+console.log(a,...b);
